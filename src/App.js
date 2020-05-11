f646b2eb10c16e3fef1360c20761cb401e817b9b
Victor Garcia <victorgrubiodl@gmail.com>
2020-05-11 22:32:31 +0200

    Created app and updated both css and html from videos. Dynamic stars

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [stars, setStars] = React.useState(utils.random(1, 9));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          {utils.range(1, stars).map(startId => 
+            <div key={startId} className="star" />
+          )}
+        </div>
+        <div className="right">
+          {utils.range(1, 9).map(number => 
+            <button key={number} className="number">{number}</button>
+          )}
+        </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
     </div>
   );
-}
+};
+
+
+/*
+Video 1
+-------
+We have to create the stars as a state item as it will
+be an UI element that wil change its value
+
+We use the jsx syntax and the range function that creates an
+Array from min to max values with step of 1. Then we map this
+array into a div creation for both stars and numbers
+
+
+Video 2
+-------
+*/
+
+
+// Color Theme
+const colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
+
+// Math science
+const utils = {
+  // Sum an array
+  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+
+  // create an array of numbers between min and max (edges included)
+  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+  // pick a random number between min and max (edges included)
+  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+
+  // Given an array of numbers and a max...
+  // Pick a random sum (< max) from the set of all available sums in arr
+  randomSumIn: (arr, max) => {
+    const sets = [[]];
+    const sums = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0, len = sets.length; j < len; j++) {
+        const candidateSet = sets[j].concat(arr[i]);
+        const candidateSum = utils.sum(candidateSet);
+        if (candidateSum <= max) {
+          sets.push(candidateSet);
+          sums.push(candidateSum);
+        }
+      }
+    }
+    return sums[utils.random(0, sums.length - 1)];
+  },
+};
 
 export default App;
