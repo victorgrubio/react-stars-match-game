4004a08ea0f69d4c962159ba97a350b0a7dfd267
Victor Garcia <victorgrubiodl@gmail.com>
2020-05-15 17:43:24 +0200

    Finished the game

--- Notes.md
+++ Notes.md
@@ -11,4 +11,12 @@ We have to split the stars and the buttonNumbers as different components to have
 We have implemented the states for the different numbers: Available or candidate, as wrong or used can be deducted from them. Then we can style them using the jsx styling functions learn. 
 
 ## Video 4
-s
+We have implemented the reset game functionality to restart it when the player has completed it.
+
+## Unmount
+Now we have removed the reset functionality and substitute it by using the mount/unmount functionality. To do so, we have labeled our game with an ID and each time the game finishes, it gets a new (different, using increment) id and restarts the state values to their originals.
+
+NOTE: The timer functionality does not work properly at it doesn't follow the counter proportion (i.e. a regular times shows 8s seconds consumed where the game timer shows 2 (8 seconds left)).
+
+## Use custom hooks
+As the Game component now contains a lot of functionalities, we should create a custom hook to be the manager of the game and let the Game component to handle only some small computations and the UI.
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 const StarsDisplay = props => (
@@ -18,37 +18,41 @@ const PlayNumber = props => (
   </button>
 );
 
+const PlayAgain = props => (
+  <div className="game-done">
+    <div 
+      className="message"
+      style={{color: props.gameStatus === 'won' ? 'green' : 'red'}}
+    >
+      {props.gameStatus === 'won' ? 'You have won!' : 'Game Over'}
+    </div>
+    <button onClick={props.onClick}>
+      Play Again!
+    </button>
+  </div>
+);
 
-const App = () => {
+// Custom hook that manages the game
+const useGameState = timeLimit => {
+
+  /* Definition states */
   const [stars, setStars] = React.useState(utils.random(1, 9));
   const [availableNums, setAvalailableNums] = React.useState(utils.range(1, 9))
   const [candidateNums, setCandidateNums] = React.useState([])
+  const [secondsLeft, setSecondsLeft] = React.useState(10)
 
-  // Our candidate numbers are wrong if its sum is greater
-  // than the number of stars
-  const candidatesAreWrong = utils.sum(candidateNums) > stars;
-
-  // We have to get the status for each number
-  const numberStatus = (number) => {
-    if(!availableNums.includes(number))
-      return 'used'
-    if(candidateNums.includes(number))
-      return candidatesAreWrong ? 'wrong' : 'candidate'
-    return 'available'
-  }
-
-  const onNumberClick = (number, currentStatus) => {
-    // If it has been used return
-    if (currentStatus === 'used'){
-      return ; 
+  /* Use effect function */
+  useEffect(() => {
+    // Use timer if the game is active only
+    if (secondsLeft > 0 && availableNums.length > 0 ){
+      const timerId = setTimeout(() => {
+        setSecondsLeft(secondsLeft - 1);
+      }, 1000);
+      return () => clearTimeout(timerId);
     }
-    // new candidates number: If it is available then concat
-    // else: filter if the candidate num is not in candidates to remove it
-    const newCandidateNums = 
-      currentStatus === 'available' 
-      ? candidateNums.concat(number)
-      : candidateNums.filter(candidateNumber => candidateNumber !== number)
-    
+  });
+
+  const setGameState = (newCandidateNums) => {
     // checks if the sum of numbers is equal to the stars
     if (utils.sum(newCandidateNums) !== stars){
       // if it isn't add new candidate
@@ -64,8 +68,59 @@ const App = () => {
       setAvalailableNums(newAvailableNums);
       // clear candidates
       setCandidateNums([])
-
     }
+  }
+  return { stars, availableNums, candidateNums, secondsLeft, setGameState };
+}
+
+
+
+const Game = (props) => {
+
+  /* Now all the state data comes from the Manager hook */
+  const {
+    stars,
+    availableNums,
+    candidateNums,
+    secondsLeft,
+    setGameState,
+  } = useGameState();
+
+  /* Constants for the app */
+  // Our candidate numbers are wrong if its sum is greater
+  // than the number of stars
+  const candidatesAreWrong = utils.sum(candidateNums) > stars;
+
+  // Checks status: first check if counter is finished then 
+  // checks if we have any available num
+  const gameStatus = secondsLeft === 0 ? 'lost' :
+    availableNums.length === 0 ? 'won' : 'active'
+
+  /* Common app functions */
+  // We have to get the status for each number
+  const numberStatus = (number) => {
+    if(!availableNums.includes(number))
+      return 'used'
+    if(candidateNums.includes(number))
+      return candidatesAreWrong ? 'wrong' : 'candidate'
+    return 'available'
+  }
+
+  // Clicking function for numbers
+  const onNumberClick = (number, currentStatus) => {
+    // If it has been used and we can play, return
+    if (currentStatus === 'used' || secondsLeft === 0){
+      return ; 
+    }
+    // new candidates number: If it is available then concat
+    // else: filter if the candidate num is not in candidates to remove it
+    const newCandidateNums = 
+      currentStatus === 'available' 
+      ? candidateNums.concat(number)
+      : candidateNums.filter(candidateNumber => candidateNumber !== number)
+    
+      // Updates the game's state with the new candidate numbers
+      setGameState(newCandidateNums);
   }
 
   return (
@@ -75,7 +130,13 @@ const App = () => {
       </div>
       <div className="body">
         <div className="left">
-          <StarsDisplay count={stars} />
+          {gameStatus !== 'active' ? (
+          <PlayAgain 
+          onClick={props.startNewGame}
+          gameStatus={gameStatus}
+          />) : (
+           <StarsDisplay count={stars} />
+           )}
         </div>
         <div className="right">
           {utils.range(1, 9).map(number => 
@@ -88,11 +149,16 @@ const App = () => {
           )}
         </div>
       </div>
-      <div className="timer">Time Remaining: 10</div>
+      <div className="timer">Time Remaining: {secondsLeft}</div>
     </div>
   );
 };
 
+const App = () => {
+  
+  const [gameId, setGameId] = React.useState(1);
+  return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)}/>
+}
 
 // Color Theme
 const colors = {
